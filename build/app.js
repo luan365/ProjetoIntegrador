e580f3a52a469981dee488b0fcf3bcb72a8dc8c8
luan365 <62581098+luan365@users.noreply.github.com>
2023-10-03 14:35:38 -0300

    comit

--- build/app.js
+++ build/app.js
@@ -6,17 +6,18 @@ import dotenv from "dotenv";
 // configurar o servidor web com express
 const app = express();
 const port = 3000;
-//daleto JSON para REQ e RES
+// daleto JSON para REQ e RES
 app.use(express.json());
-
+// Criando uma conexão
+const conn = await oracleDB.getConnection({
+    user: "ADMIN",
+    password: "Puc123456789",
+    connectionString: "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.sa-saopaulo-1.oraclecloud.com))(connect_data=(service_name=gf8f07257ad5e78_projetointegrador_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))"
+});
 // Serviços de backend
 app.get("/obterAeronaves", async(req,res) =>{
     // 1- Abrir conexão com o Oracle
-    const conn = await oracleDB.getConnection({
-        user: "ADMIN",
-        password: "123456",
-        connectionString: "oegar string no OCI(Oracle cloud) - HIGH"
-    });
+    conn;
     // 2- Buscar as aeronaves(select)
     const results = await conn.execute("SELECT * FROM Aeronaves");
 
@@ -27,15 +28,21 @@ app.get("/obterAeronaves", async(req,res) =>{
 });
 app.put("/inserirAeronave", (req,res) =>{
     // 1- Abrir conexão com o Oracle
+    conn;
     // 2- Inserir as aeronaves(insert)
     // 3- Fechar conexão com o Oracle
+    await conn.close();
     // 4- Devolver resultados
+    res.send(results);
 });
 app.get("/excluirAeronaves", (req,res) =>{
     // 1- Abrir conexão com o Oracle
+    conn;
     // 2- Excluir as aeronaves(delete)
     // 3- Fechar conexão com o Oracle
-    // 4- Devolver resultados 
+    await conn.close();
+    // 4- Devolver resultados
+    res.send(results);
 });
 app.listen(port, () => {
     console.log("Servidor HTTP inicializado...");
