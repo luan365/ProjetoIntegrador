aafbda67e2f28975eeb0d4809cb1454b01c76bf0
Luan Ferreira <luanferreira0914@gmail.com>
2023-10-05 11:15:26 -0300

    commit

--- README.md
+++ README.md
@@ -13,7 +13,6 @@ para usar o oracle pelo sqldeveloper, conectar via wallet
 para usar o oracle no node (ou em qualquer softwsare que eu criar) eu uso via string de conexão - uma linguiça de texto que tem os dados de conexão.
 
 
-
 Tarefa pra casa{
   1- Criar conta na OCI e subir um banco
   2- Criar a tabela AERONAVES  e colocar alumas via SQL
@@ -22,4 +21,5 @@ Tarefa pra casa{
   5- Tirar a SENHA e USUÁRIO do CÓDIGO... usando o dotenv (criar um arquivo de ambiente e criar o usuário e a senha dele, e no código mandar buscar pelo arquivo, para aumentar a segurança)
   6- Estudar o que são Promises Javascript/Typescrpit(async/await)
   7- Todo serviço que envolve banco de dados deve trabalhar com Promises
+  8- estudar o q é fetchInserir no codigo do professor e entregar um relatório
 }
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,132 @@
 //importação dos moduloes necessários
-import oracleDB from "oracledb";
 import express  from "express";
-import dotenv from "dotenv";
+import oracleDB, {Connection, ConnectionAttributes} from "oracledb";
+import dotenv from "dotenv"; // Dados sensíceis é colodado no arquivo dotEnv (dot - ponto(.))
+import cors from "cors"; // Serve para liberar/restringir acesso ao serviços
 
-// configurar o servidor web com express
+// configurar o servidor web com express na porta 3000
 const app = express();
 const port = 3000;
-//daleto JSON para REQ e RES
-app.use(express.json());
 
-//Serviços de backend
-app.get("/listarAeronaves")
+dotenv.config(); // "Vou usar o dotenv"
+// preparar o servidor JSON para REQ e RES
+app.use(express.json());
+app.use(cors());
+
+type CustomResponse = {
+    status: string,
+    message: string,
+    payload: any
+};
+
+type Aeronave = {
+    fabricante: string,
+    qtdeAssentos: number,
+    modelo: string;
+};
+
+
+// Serviços de backend
+app.get("/obterAeronaves", async(req,res) =>{
+    let cr: CustomResponse = {
+        status: "ERROR",
+        message: "",
+        payload: undefined,
+    };
+
+        //conectando
+        let conn;
+        try{
+            conn = await oracleDB.getConnection({
+                user: process.env.ORACLE_DB_USER,
+                password: process.env.ORACLE_DB_PASSWORD,
+                connectionString: process.env.ORACLE_DB_STR
+            });
+        }catch(e){
+            if(e instanceof Error){
+                cr.message = e.message;
+                console.log(e.message)
+            }
+        }finally{
+            if(conn !== undefined){
+                await conn.close();
+            }
+            // 4- Devolver resultados
+            res.send(cr)
+        }   
+    // 2- Buscar as aeronaves(select)
+    const results = await conn.execute("SELECT * FROM Aeronaves");
+
+    // 3- Fechar conexão com o Oracle
+    await conn.close();
+});
+app.put("/InserirAeronave", async(req,res) => {
+    const marca = req.body.marca as string;
+    const modelo = req.body.modelo as string;
+    const qtdeAssentos = req.body.qtdeAssentos as number;
+    const registro = req.body.registro as string;
+
+    //correção: verificar se tudo chegoupara prosseguir com o cadastro
+    //verificar se chegaram os parametros
+    //VALIDAR se estão bons (qtdeAssentos não pode ser negativo)
+
+    let cr: CustomResponse = {
+        status: "ERROR",
+        message: "",
+        payload: undefined,
+    };
+
+    
+    //conectando
+    let conn; //let = variavel local | const = Variavel que nao muda | var = Variavel global - pode ser modificado em qualquer ponto do programa
+    try{
+        conn = await oracleDB.getConnection({
+            user: process.env.ORACLE_DB_USER,
+            password: process.env.ORACLE_DB_PASSWORD,
+            connectionString: process.env.ORACLE_DB_STR,
+        });
+    }catch(e){
+    }finally{}   
+
+    conn.close;
+
+});
+app.get("/excluirAeronaves", (req,res) =>{
+    
+    let cr: CustomResponse = {
+        status: "ERROR",
+        message: "",
+        payload: undefined,
+    };
+
+        //conectando
+        let conn;
+        try{
+            conn = await oracleDB.getConnection({
+                user: process.env.ORACLE_DB_USER,
+                password: process.env.ORACLE_DB_PASSWORD,
+                connectionString: process.env.ORACLE_DB_STR
+            });
+        }catch(e){
+            if(e instanceof Error){
+                cr.message = e.message;
+                console.log(e.message)
+            }
+        }finally{
+            if(conn !== undefined){
+                await conn.close();
+            }
+            res.send(cr)
+        }   
+    // 2- Excluir as aeronaves(delete)
+
+    // Commit para gravar no oracle
+    await conn.commit();
+    // 3- Fechar conexão com o Oracle
+    await conn.close();
+    // 4- Devolver resultados
+    res.send(results);
+});
+app.listen(port, () => {
+    console.log("Servidor HTTP inicializado...");
+});
